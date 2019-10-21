cb157fc175f1715ab49a6bc8f4eaf3100975b219
shaxiaoqin <shaxiaoqin@139.com>
2019-10-21 15:36:05 +0800

    aa

--- zft.py
+++ zft.py
@@ -12,8 +12,17 @@ if __name__ == '__main__':
     cv2.imshow('img1', imgobj1)
     cv2.waitKey(0)
 
-		#�Ҷ�ͼ
+    # 灰度
     gray = cv2.cvtColor(imgobj1, cv2.COLOR_BGR2GRAY)
     cv2.imshow('img2', gray)
-    gray.save('12306.Grayscale.png')
+
+    img = cv2.imread("12306.png", cv2.IMREAD_GRAYSCALE)
+    cv2.imwrite('12306.Grayscale.png', img)
+
     cv2.waitKey(0)
+
+    # 二值
+    img = cv2.imread("12306.png")
+    Grayimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(Grayimg, 150, 255, cv2.THRESH_BINARY)
+    cv2.imwrite('12306.BINARY.png', thresh)
